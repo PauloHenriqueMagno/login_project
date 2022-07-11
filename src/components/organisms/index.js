f56a47e27d18966a4ed0c96187c7543e604ecf24
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-07-11 15:50:10 -0300

    feat(components/organisms): exported all the organims components

---
+++ src/components/organisms/index.js
@@ -0,0 +1,7 @@
+import ForgotPasswordFormComponent from './ForgotPasswordFormComponent';
+import LoginFormComponent from './LoginFormComponent';
+
+export {
+  ForgotPasswordFormComponent,
+  LoginFormComponent
+}
