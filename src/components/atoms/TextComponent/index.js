7840276e09b62be8f4748bf5eb15df7e88c71f32
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-07-11 15:42:48 -0300

    feat(components/atoms): create TextComponent

---
+++ src/components/atoms/TextComponent/index.js
@@ -0,0 +1,12 @@
+import { styled } from "@material-ui/core";
+
+const TextComponent = styled('p')({
+  color: '#525357',
+  fontSize: '1.15rem',
+  fontWeight: '500',
+  margin: 'auto',
+  width: '100%',
+  textAlign: 'center'
+});
+
+export default TextComponent;
