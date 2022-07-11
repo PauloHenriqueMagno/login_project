3412121afbbc76197c725e9e710a96862001ba7a
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-07-11 15:54:01 -0300

    feat(pages): assemble the login page components

---
+++ src/pages/LoginPage/index.js
@@ -0,0 +1,12 @@
+import { BackgroundComponent } from '../../components/atoms';
+import { LoginFormComponent } from '../../components/organisms';
+
+const LoginPage = () => {
+  return (
+    <BackgroundComponent>
+      <LoginFormComponent />
+    </BackgroundComponent>
+  );
+}
+
+export default LoginPage;
