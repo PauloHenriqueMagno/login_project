2d8446b16e6428e9b07043c5a669466cab7eaa0b
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-07-11 15:35:17 -0300

    feat(components/atoms): create FormComponent

---
+++ src/components/atoms/FormComponent/index.js
@@ -0,0 +1,12 @@
+import { styled } from '@material-ui/core';
+
+export const FormComponent = styled('form')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  gap: '40px'
+});
+
+export default FormComponent;
