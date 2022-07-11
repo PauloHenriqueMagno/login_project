129965ee6a0a0e4437250b439bb6dea7bc309453
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-07-11 15:44:10 -0300

    feat: export all atoms components

---
+++ src/components/atoms/index.js
@@ -0,0 +1,15 @@
+import TextComponent from './TextComponent';
+import WhiteBoxComponent from './WhiteBoxComponent';
+import ButtonComponent from './ButtonComponent';
+import FormComponent from './FormComponent';
+import LabelComponent from './LabelComponent';
+import BackgroundComponent from './BackgroundComponent';
+
+export {
+  TextComponent,
+  WhiteBoxComponent,
+  ButtonComponent,
+  FormComponent,
+  LabelComponent,
+  BackgroundComponent
+}
